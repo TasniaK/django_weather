33f6b144f5fdc68dd38f2bc178d9e440f431f248
Tasnia <tasniakamal@hotmail.co.uk>
2019-06-30 22:39:03 +0100

    added weather app (rest api) that returns data from public weather api, removed .pyc files

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+import requests
+
+def weather_data(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8a34f3f503dfd53e88c467e4267b9cc9'
+    weather = requests.get(url).json()
+
+    return JsonResponse(weather)
