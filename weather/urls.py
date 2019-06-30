e6ece8331623f072eb00a4ff59d95ba9a916030d
Tasnia <tasniakamal@hotmail.co.uk>
2019-06-30 22:54:54 +0100

    allow city paramter to be passed into url to return weather data for that city

--- weather/urls.py
+++ weather/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('weather', views.weather_data, name='weather_data'),
+    path('weather/', views.weather_data, name='weather_data'),
 
 ]
--- weather/views.py
+++ weather/views.py
@@ -4,6 +4,7 @@ import requests
 
 def weather_data(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8a34f3f503dfd53e88c467e4267b9cc9'
-    weather = requests.get(url).json()
+    city = request.GET.get('city', '')
+    weather = requests.get(url.format(city)).json()
 
     return JsonResponse(weather)
