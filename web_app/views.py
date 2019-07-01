f9dc1ef9b1208e410ed2d1abc4183acb3d8e112f
Tasnia <tasniakamal@hotmail.co.uk>
2019-07-01 09:00:31 +0100

    return london data for now, return data from my weather api into homepage view

--- web_app/views.py
+++ web_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 from django.shortcuts import render
+import requests
 
 def index(request):
+    city_weather = requests.get('http://127.0.0.1:8000/api/weather/?city=london').json()
+
     weather = {
-        'city': 'Las Vegas',
+        'city': 'London',
         'temperature': city_weather['main']['temp'],
         'description': city_weather['weather'][0]['description'],
         'icon': city_weather['weather'][0]['icon']
