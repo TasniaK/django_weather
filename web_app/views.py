3ce4a70035866d377b26720b2599e24f9ff9aac2
Tasnia <tasniakamal@hotmail.co.uk>
2019-07-01 19:45:31 +0100

    added user input field using django forms.

--- web_app/views.py
+++ web_app/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
 import requests
+from .forms import CityForm
 
 def index(request):
-    city_weather = requests.get('http://127.0.0.1:8000/api/weather/?city=london').json()
+    if request.method == 'POST':
+        # create a form instance and populate with data form thr request
+        form = CityForm(request.POST)
 
-    weather = {
-        'city': 'London',
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
+        if form.is_valid():
+            user_input = form.data
+            city = user_input.get('city')
+            city_weather = requests.get('http://127.0.0.1:8000/api/weather/?city={}'.format(city)).json()
+            weather = {
+                'city': city_weather['name'],
+                'temperature': city_weather['main']['temp'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon']
+            }
+            context = {'weather': weather, 'form': form}
+            return render(request, 'weather/index.html', context)
+        else:
+            return render(request, 'weather/index.html', {'form': form})
 
-    context = {'weather': weather}
-    return render(request, 'weather/index.html', context)
+        # if GET or any other method
+    else:
+        form = CityForm()
+        return render(request, "weather/index.html", {'form': form})
